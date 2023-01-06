5e219af89024dd7e0577900a075587198228f8ea
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-01-06 18:57:13 +0100

    get value of the form input/output value

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -4,3 +4,9 @@ window.addEventListener("load", async function() {
     const currentAmount = await dbank_backend.checkBalance();
     document.getElementById("value").innerText = Math.round(currentAmount * 100);
 });
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+    event.preventDefault();
+    const inputAmount = document.getElementById("input-amount").ariaValueMax;
+    const outputAmount = document.getElementById("withdrawal-amount").ariaValueMax;
+})
