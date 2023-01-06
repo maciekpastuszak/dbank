bff3f4b4befd4604b7d8b08a2ac914fc8b02b393
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-01-06 18:53:37 +0100

    round the currentValue

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,6 +1,6 @@
 import { dbank_backend } from "../../declarations/dbank_backend";
 
 window.addEventListener("load", async function() {
-    const currentAmount = dbank_backend.checkBalance();
-    this.document.getElementById("value").innerText = currentAmount;
-})
+    const currentAmount = await dbank_backend.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount * 100);
+});
