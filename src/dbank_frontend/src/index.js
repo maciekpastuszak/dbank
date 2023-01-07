52298dee946271e25f61e95e08a5f62064dc8c23
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-01-07 19:29:48 +0100

    disable button for finalizing when under transaction

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -7,11 +7,18 @@ window.addEventListener("load", async function() {
 
 document.querySelector("form").addEventListener("submit", async function(event) {
     event.preventDefault();
+    
+    const button = event.target.querySelector("#submit-btn ")
+
     const inputAmount = parseFloat(document.getElementById("input-amount").value);
     const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+    button.setAttribute("disabled", true);
 
     await dbank_backend.topUp(inputAmount);
 
     const currentAmount = await dbank_backend.checkBalance();
     document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+
+    button.removeAttribute("disabled");
 })
